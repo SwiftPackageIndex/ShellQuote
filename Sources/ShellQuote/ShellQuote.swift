ca6a456553ee89168f4a680d443358a2f6f36d29
Sven A. Schmidt <sas@finestructure.co>
2023-03-11 09:09:45 +0100

    Basic implementation

--- Sources/ShellQuote/ShellQuote.swift
+++ Sources/ShellQuote/ShellQuote.swift
@@ -1,6 +1,20 @@
-public struct ShellQuote {
-    public private(set) var text = "Hello, World!"
+struct ShellQuote {
+    private static let unsafe = try! Regex(#"[^\w@%+=:,./-]"#)
 
-    public init() {
+    static func hasUnsafeContent(_ input: String) -> Bool {
+        input.firstMatch(of: unsafe) != nil
+    }
+
+    /// Return a shell-escaped version of the input string.
+    ///
+    /// The input string will be wrapped in single quotes (') and all existing single quotes within the string
+    /// will be escaped via double quotes: "'".
+    ///
+    /// For example, $'b will be quoted as '$'"'"'b'.
+    /// - Parameter input: input string
+    /// - Returns: shell-escaped output string
+    static func quote(_ input: String) -> String {
+        if !hasUnsafeContent(input) { return input }
+        return "'" + input.replacing("'", with: #"'"'"'"#) + "'"
     }
 }
--- Tests/ShellQuoteTests/ShellQuoteTests.swift
+++ Tests/ShellQuoteTests/ShellQuoteTests.swift
@@ -2,10 +2,42 @@ import XCTest
 @testable import ShellQuote
 
 final class ShellQuoteTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(ShellQuote().text, "Hello, World!")
+
+    func test_hasUnsafeContent() throws {
+        XCTAssertFalse(ShellQuote.hasUnsafeContent(""))
+        XCTAssertFalse(ShellQuote.hasUnsafeContent("abc"))
+        XCTAssertFalse(ShellQuote.hasUnsafeContent("@"))
+        XCTAssertFalse(ShellQuote.hasUnsafeContent("%"))
+        XCTAssertFalse(ShellQuote.hasUnsafeContent("+"))
+        XCTAssertFalse(ShellQuote.hasUnsafeContent("="))
+        XCTAssertFalse(ShellQuote.hasUnsafeContent(":"))
+        XCTAssertFalse(ShellQuote.hasUnsafeContent(","))
+        XCTAssertFalse(ShellQuote.hasUnsafeContent("."))
+        XCTAssertFalse(ShellQuote.hasUnsafeContent("/"))
+        XCTAssertFalse(ShellQuote.hasUnsafeContent("-"))
+
+        XCTAssertTrue(ShellQuote.hasUnsafeContent(";"))
+        XCTAssertTrue(ShellQuote.hasUnsafeContent("&"))
+        XCTAssertTrue(ShellQuote.hasUnsafeContent(">"))
+        XCTAssertTrue(ShellQuote.hasUnsafeContent("<"))
+        XCTAssertTrue(ShellQuote.hasUnsafeContent("!"))
+        XCTAssertTrue(ShellQuote.hasUnsafeContent("("))
+        XCTAssertTrue(ShellQuote.hasUnsafeContent(")"))
+        XCTAssertTrue(ShellQuote.hasUnsafeContent("{"))
+        XCTAssertTrue(ShellQuote.hasUnsafeContent("}"))
+        XCTAssertTrue(ShellQuote.hasUnsafeContent("["))
+        XCTAssertTrue(ShellQuote.hasUnsafeContent("]"))
+        XCTAssertTrue(ShellQuote.hasUnsafeContent("#"))
+        XCTAssertTrue(ShellQuote.hasUnsafeContent("$"))
+        XCTAssertTrue(ShellQuote.hasUnsafeContent("^"))
+        XCTAssertTrue(ShellQuote.hasUnsafeContent("*"))
     }
+
+    func test_quote() throws {
+        XCTAssertEqual(ShellQuote.quote("$'b"), #"'$'"'"'b'"#)
+        XCTAssertEqual(ShellQuote.quote(""), #""#)
+        XCTAssertEqual(ShellQuote.quote("abc"), #"abc"#)
+        XCTAssertEqual(ShellQuote.quote(";"), #"';'"#)
+    }
+
 }
